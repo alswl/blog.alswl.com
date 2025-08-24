0c3f9b3e48a27659b7e114471626a0f62abd8e2f
Jingchao <alswlx@gmail.com>
2025-08-24 10:31:55 +0800

    feat: new pub

--- hack/resize-images-in-git-workdir.sh
+++ hack/resize-images-in-git-workdir.sh
@@ -2,5 +2,5 @@
 
 for i in $(git status | grep images | awk '{print $3}'); do
     echo resize "$i";
-    convert "$i" -strip -auto-orient -resize 1000x1000\> "$i";
+    magick "$i" -strip -auto-orient -resize 1000x1000\> "$i";
 done
